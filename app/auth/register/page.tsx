7289df347ec0449258795531145153ae8e0191b8
Alew <xalejandrow@gmail.com>
2025-03-11 17:48:15 -0300

    Añadiendo Metadata a las páginas

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,13 +1,19 @@
+import type { Metadata } from "next";
 import RegisterForm from "@/components/auth/RegisterForm";
+
+export const metadata : Metadata = {
+  title: "CashTrackr - Crear una cuenta",
+  description: "CashTrackr - Crear una cuenta",
+};
 
 export default function RegisterPage() {
     
   return (
     <>
-      <h1 className="font-black text-6xl text-purple-950">Crear una cuenta</h1>
-      <p className="text-3xl font-bold">
-        y controla tu <span className="text-amber-500">finanzas</span>
-      </p>
+        <h1 className="font-black text-6xl text-purple-950">Crear una cuenta</h1>
+        <p className="text-3xl font-bold">
+            y controla tu <span className="text-amber-500">finanzas</span>
+        </p>
 
         <RegisterForm />
       
