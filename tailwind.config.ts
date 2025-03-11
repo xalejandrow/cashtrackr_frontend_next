42056d3a7481fe8d942e48752d3cffc15020d51d
Alew <xalejandrow@gmail.com>
2025-03-11 20:27:40 -0300

    Añadiendo un gráfico al layout de Autenticación con Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,16 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        "auth": "url('/grafico.svg')"
+      },
+      backgroudSize: {
+        '30': '30rem'
+      }
+    },
+  },
   plugins: [],
 };
 export default config;
