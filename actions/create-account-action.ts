44045cb43859639016ba15b6907784f67bba9a0a
Alew <xalejandrow@gmail.com>
2025-03-12 15:54:01 -0300

    Creando y ejecutando el primer Server Action

---
+++ actions/create-account-action.ts
@@ -0,0 +1,6 @@
+"use server"
+
+export async function register() {
+    console.log('Desde Server Action');
+    
+}
--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { register } from "@/actions/create-account-action";
+
 export default function RegisterForm() {
   return (
-    <form className="mt-14 space-y-5" noValidate>
+    <form 
+      className="mt-14 space-y-5" 
+      noValidate
+      action={register}
+    >
       <div className="flex flex-col gap-2">
         <label className="font-bold text-2xl" htmlFor="email">
           Email
