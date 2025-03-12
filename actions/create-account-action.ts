573bd7dc244b675094e39bfee1ce2104d98517e2
Alew <xalejandrow@gmail.com>
2025-03-12 16:07:43 -0300

    Creando un objeto para enviar hacia el backend

--- actions/create-account-action.ts
+++ actions/create-account-action.ts
@@ -2,5 +2,15 @@
 
 export async function register(formData: FormData) {
     console.log(formData);
+
+    const registerData = {
+        email: formData.get("email"),
+        name: formData.get("name"),
+        password: formData.get("password"),
+        password_confirmation: formData.get("password_confirmation")
+    }
+
+    console.log(registerData);
+    
     
 }
