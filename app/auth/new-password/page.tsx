6a36d5f554dd517ab63961802f4d0fcfcba70318
Alew <xalejandrow@gmail.com>
2025-03-27 17:18:19 -0300

    Creando los Formularios para ingresar token y nuevo password

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -1,9 +1,18 @@
+import PasswordResetHandler from "@/components/auth/PasswordResetHandler";
 
 export default function NewPasswordPage() {
-    return (
-      <div>
-        NewPasswordPage
-      </div>
-    )
-  }
-  
+  return (
+	<>
+		<h1 className="font-black text-6xl text-purple-950">
+			Reestablecer Password
+		</h1>
+		<p className="text-3xl font-bold">
+			Ingresa el código que recibiste
+			<span className="text-amber-500"> por email</span>
+		</p>
+
+		<PasswordResetHandler />
+		
+	</>
+  );
+}
