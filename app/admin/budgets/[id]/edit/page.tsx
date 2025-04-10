c66bae2bdfbc810f108a7cb6d2ca8da75c19dbbf
Alew <xalejandrow@gmail.com>
2025-04-10 11:59:13 -0300

    Moviendo la función hacia un archivo aparte

--- app/admin/budgets/[id]/edit/page.tsx
+++ app/admin/budgets/[id]/edit/page.tsx
@@ -1,41 +1,7 @@
 import { Metadata } from "next";
-import { cache } from "react";
 import Link from "next/link";
-import { notFound } from "next/navigation";
 import EditBudgetForm from "@/components/budgets/EditBudgetForm";
-import getToken from "@/src/auth/token";
-import { BudgetAPIResponseSchema } from "@/src/schemas";
-
-
-// La función cache es una función de Next.js que permite almacenar en caché los resultados de una función asíncrona.
-// Esto es útil para evitar hacer múltiples solicitudes a la misma API y mejorar el rendimiento de la aplicación.
-// La función cache toma una función como argumento y devuelve una nueva función que almacena en caché los resultados de la función original.
-// En este caso, estamos utilizando la función cache para almacenar en caché los resultados de la función getBudget. (no es necesaria en versiones más nuevas de Next.js)
-const getBudget = cache (async (budgetId: string) => {
-// const getBudget = async (budgetId: string) => {
-    // console.log(budgetId);
-    const token = await getToken();
-    const url = `${process.env.API_URL}/budgets/${budgetId}`;
-    const req = await fetch(url, {
-        method: "GET",
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        },
-    });
-    const json = await req.json();
-    // console.log(json);
-    if(!req.ok) {
-        notFound();
-    }
-
-    // console.log('json en getBudget: ',json);
-    // return json;
-    const budget = BudgetAPIResponseSchema.parse(json);
-    // console.log('budget en getBudget: ',budget);
-    return budget;
-// }
-})
+import { getBudget } from "@/src/services/budgets";
 
 export async function generateMetadata({params} : {params: Promise<{id: string}>}) : Promise<Metadata> {
 
---
+++ src/services/budgets.ts
@@ -0,0 +1,28 @@
+// La función cache es una función de Next.js que permite almacenar en caché los resultados de una función asíncrona.
+// Esto es útil para evitar hacer múltiples solicitudes a la misma API y mejorar el rendimiento de la aplicación.
+// La función cache toma una función como argumento y devuelve una nueva función que almacena en caché los resultados de la función original.
+
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import getToken from "../auth/token";
+import { BudgetAPIResponseSchema } from "../schemas";
+
+// En este caso, estamos utilizando la función cache para almacenar en caché los resultados de la función getBudget. (no es necesaria en versiones más nuevas de Next.js)
+export const getBudget = cache (async (budgetId: string) => {
+        const token = await getToken();
+        const url = `${process.env.API_URL}/budgets/${budgetId}`;
+        const req = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        const json = await req.json();
+        if(!req.ok) {
+            notFound();
+        }
+
+        const budget = BudgetAPIResponseSchema.parse(json);
+        return budget;
+    })
