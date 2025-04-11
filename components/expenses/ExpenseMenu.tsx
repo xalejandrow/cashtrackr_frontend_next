d0eb1a3d26f2ee9bea96f9ea41ad380de2e65282
Alew <xalejandrow@gmail.com>
2025-04-11 01:06:39 -0300

    Mostrando el Formulario

--- components/expenses/ExpenseMenu.tsx
+++ components/expenses/ExpenseMenu.tsx
@@ -35,7 +35,7 @@ export default function ExpenseMenu({expenseId}: {expenseId: Expense['id']}) {
                     <button
                         type="button"
                         className="block px-3 py-1 text-sm leading-6 text-gray-900"
-                        onClick={() => router.push(location.pathname + `?showModal=true&ExpenseId=${expenseId}`)}
+                        onClick={() => router.push(location.pathname + `?showModal=true&editExpenseId=${expenseId}`)}
                     >
                         Editar Gasto
                     </button>
