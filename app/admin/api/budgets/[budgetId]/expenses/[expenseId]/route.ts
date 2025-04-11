94c57e8e4b24f10aaddea816b81ed35a7c1e2547
Alew <xalejandrow@gmail.com>
2025-04-11 01:35:17 -0300

    Consultando la API para obtener el presupuesto (API Route)

--- app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
+++ app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,7 +1,24 @@
 import { verifySession } from "@/src/auth/dal"
+import getToken from "@/src/auth/token"
 
-export async function GET(){
+export async function GET(request: Request,{params}: {params: Promise<{budgetId: string, expenseId: string}>}) {
 
     await verifySession() 
-    return Response.json('Hola Mundo')
+    const { budgetId, expenseId } = await params
+    // console.log('params: ', budgetId);
+    // console.log('params: ', expenseId);
+    const token = await getToken()
+    const url = `${process.env.API_URL}/budgets/${budgetId}/expenses/${expenseId}`
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    const json = await req.json()
+
+    if (!req.ok) {
+        return Response.json(json.error, { status: 403 })
+    }
+    
+    return Response.json(json)
 }
